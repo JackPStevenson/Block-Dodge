76ca9a80c1c4e3c2331c06aaf064a9dc1c4071d5
league-workshopper-2 <league@iMac.attlocal.net>
2015-10-15 19:54:25 -0700

    Finally Added Sounds!

--- src/SpaceInvadorz.java
+++ src/SpaceInvadorz.java
@@ -11,6 +11,7 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
 
@@ -20,12 +21,15 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener {
+public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
+{
 	URL backgroundURL;
 	URL backgroundURL2;
 	Image backgroundSky;
@@ -49,6 +53,8 @@ public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
 	int bkgX2 = 0;
 	int bkgY2 = -500;
 	int dSTime;
+	Sound Explo1;
+	Sound Explo2;
 	Random xRandom;
 	Random xRandom2;
 	static int gameState = 0;
@@ -58,36 +64,44 @@ public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
 	static int highScore;
 	static int lives = 2;
 	boolean dShield = false;
+	boolean soundFile = false;
 
-	public static int getGameState() {
+	public static int getGameState()
+	{
 		return gameState;
 	}
 
-	public static void setGameState(int a) {
+	public static void setGameState(int a)
+	{
 		gameState = a;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Toolkit.getDefaultToolkit();
 		SpaceInvadorz Start = new SpaceInvadorz();
 		Start.MainStart();
 	}
 
-	public void MainStart() {
+	public void MainStart()
+	{
 		xRandom = new Random();
 		xRandom2 = new Random();
 		startFrame = new JFrame("Start Frame");
-		try {
-			startFrame.setContentPane(new JPanel() {
-				BufferedImage backgroundImage = ImageIO
-						.read(this.getClass().getResourceAsStream("Starting Screen.png"));
+		try
+		{
+			startFrame.setContentPane(new JPanel()
+			{
+				BufferedImage backgroundImage = ImageIO.read(this.getClass().getResourceAsStream("Starting Screen.png"));
 
-				public void paintComponent(Graphics g) {
+				public void paintComponent(Graphics g)
+				{
 					super.paintComponent(g);
 					g.drawImage(backgroundImage, 0, 0, 500, 500, this);
 				}
 			});
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -97,20 +111,25 @@ public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
 		startFrame.setVisible(true);
 	}
 
-	public void MainRun() {
+	public void MainRun()
+	{
 		mainFrame = new JFrame("Game Frame");
 		mainFrame.setSize(widthF, heightF);
 		mainFrame.setVisible(true);
 		backgroundURL = getClass().getResource("blue-clouds-background-cartoon-style-clouds.png");
-		try {
+		try
+		{
 			backgroundSky = ImageIO.read(backgroundURL);
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 		backgroundURL2 = getClass().getResource("blue-clouds-background-cartoon-style-clouds.png");
-		try {
+		try
+		{
 			backgroundSky2 = ImageIO.read(backgroundURL2);
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 
@@ -119,40 +138,67 @@ public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
 		Player = new PlayerClass(225, 350, 50, 50);
 		Enemy1 = new EnemyClass(50, -50, 50, 50, Player);
 		Enemy2 = new EnemyClass(400, -50, 50, 50, Player);
+		Explo1 = new Sound("/Users/league/Block-Dodge/src/E1.wav");
+		Explo2 = new Sound("/Users/league/Block-Dodge/src/E2.wav");
 		font = new Font("ComicSansMS", Font.PLAIN, 15);
 		FPS = new Timer(1000 / 30, this);
 		FPS.start();
 
 	}
 
-	private void Update() {
+	private void Update()
+	{
 		StartScreen.HighScore.setText("High Score: " + highScore / 2);
 		Player.Update();
 		Enemy1.Update();
 		Enemy2.Update();
-		if (dPressed == true) {
+		if (dPressed == true)
+		{
 			Player.moveRight();
 		}
-		if (aPressed == true) {
+		if (aPressed == true)
+		{
 			Player.moveLeft();
 		}
 		bkgY = bkgY + BaESpeed / 2;
 		bkgY2 = bkgY2 + BaESpeed / 2;
-		if (bkgY >= 500) {
+		if (bkgY >= 500)
+		{
 			bkgY = -500;
 		}
-		if (bkgY2 >= 500) {
+		if (bkgY2 >= 500)
+		{
 			bkgY2 = -500;
 		}
-		if (Player.X > Enemy1.X - 50) {
-			if (Player.X < (Enemy1.X + 50)) {
-				if (Player.Y > Enemy1.Y) {
-					if (Player.Y < (Enemy1.Y + 50)) {
-						if (dShield == false) {
+		if (Player.X > Enemy1.X - 50)
+		{
+			if (Player.X < (Enemy1.X + 50))
+			{
+				if (Player.Y > Enemy1.Y)
+				{
+					if (Player.Y < (Enemy1.Y + 50))
+					{
+
+						if (dShield == false)
+						{
+
+							soundFile = true;
+							if (soundFile = true)
+							{
+								if (lives > 1)
+								{
+									Explo1.play();
+								} else if (lives == 1)
+								{
+									Explo2.play();
+								}
+								soundFile = false;
+							}
 							lives--;
 						}
 						dShield = true;
-						if (lives == 0) {
+						if (lives == 0)
+						{
 							score = 0;
 							lives = 2;
 							Player.X = 225;
@@ -163,16 +209,36 @@ public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
 				}
 			}
 		}
-		if (Player.X > Enemy2.X - 50) {
-			if (Player.X < (Enemy2.X + 50)) {
-				if (Player.Y > Enemy2.Y) {
-					if (Player.Y < (Enemy2.Y + 50)) {
-						if (dShield == false) {
+		if (Player.X > Enemy2.X - 50)
+		{
+			if (Player.X < (Enemy2.X + 50))
+			{
+				if (Player.Y > Enemy2.Y)
+				{
+					if (Player.Y < (Enemy2.Y + 50))
+					{
+						if (dShield == false)
+						{
+
+							soundFile = true;
+							soundFile = true;
+							if (soundFile = true)
+							{
+								if (lives > 1)
+								{
+									Explo1.play();
+								} else if (lives == 1)
+								{
+									Explo2.play();
+								}
+								soundFile = false;
+							}
 							lives--;
 						}
 
 						dShield = true;
-						if (lives == 0) {
+						if (lives == 0)
+						{
 							score = 0;
 							lives = 2;
 							Player.X = 225;
@@ -183,25 +249,32 @@ public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
 				}
 			}
 		}
-		if (dShield == true) {
+		if (dShield == true)
+		{
+
 			dSTime = dSTime + 1;
-			if (dSTime > 15) {
+			if (dSTime > 15)
+			{
 				dShield = false;
 				dSTime = 0;
 			}
 		}
-		if (Enemy1.Y > 450) {
+		if (Enemy1.Y > 450)
+		{
 			Enemy1.X = xRandom.nextInt(200);
 			Enemy1.Y = -50;
 		}
-		if (Enemy2.Y > 450) {
+		if (Enemy2.Y > 450)
+		{
 			Enemy2.X = xRandom.nextInt(200) + 250;
 			Enemy2.Y = -50;
 		}
 	}
 
-	public void paint(Graphics g) {
-		if (gameState == gameScreen) {
+	public void paint(Graphics g)
+	{
+		if (gameState == gameScreen)
+		{
 			g.drawImage(backgroundSky, bkgX, bkgY, null);
 			g.drawImage(backgroundSky2, bkgX2, bkgY2, null);
 			Player.paint(g);
@@ -214,41 +287,99 @@ public class SpaceInvadorz extends JPanel implements ActionListener, KeyListener
 		}
 	}
 
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e)
+	{
 		Update();
 		repaint();
 	}
 
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent e)
+	{
 
 	}
 
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_D) {
+	public void keyPressed(KeyEvent e)
+	{
+		if (e.getKeyCode() == KeyEvent.VK_D)
+		{
 			dPressed = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_A) {
+		if (e.getKeyCode() == KeyEvent.VK_A)
+		{
 			aPressed = true;
 		}
 	}
 
-	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_D) {
+	public void keyReleased(KeyEvent e)
+	{
+		if (e.getKeyCode() == KeyEvent.VK_D)
+		{
 			dPressed = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_A) {
+		if (e.getKeyCode() == KeyEvent.VK_A)
+		{
 			aPressed = false;
 		}
 	}
 
-	private void playSound(String fileName, String action) {
-		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
-		if (action.equals("play")) {
-			sound.loop();
-			System.out.println("play");
-		} else if (action.equals("stop")) {
-			sound.stop();
-			System.out.println("stop");
+	public class Sound
+	{
+		private Clip clip;
+
+		public Sound(String fileName)
+		{
+			// specify the sound to play
+			// (assuming the sound can be played by the audio system)
+			// from a wave File
+			try
+			{
+
+				File file = new File(fileName);
+				if (file.exists())
+				{
+					AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+					// load the sound into memory (a Clip)
+					clip = AudioSystem.getClip();
+					clip.open(sound);
+				} else
+				{
+					throw new RuntimeException("Sound: file not found: " + fileName);
+				}
+			} catch (MalformedURLException e)
+			{
+				e.printStackTrace();
+				throw new RuntimeException("Sound: Malformed URL: " + e);
+			} catch (UnsupportedAudioFileException e)
+			{
+				e.printStackTrace();
+				throw new RuntimeException("Sound: Unsupported Audio File: " + e);
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+				throw new RuntimeException("Sound: Input/Output Error: " + e);
+			} catch (LineUnavailableException e)
+			{
+				e.printStackTrace();
+				throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
+			}
+
+			// play, stop, loop the sound clip
+		}
+
+		public void play()
+		{
+			clip.setFramePosition(0); // Must always rewind!
+			clip.start();
+		}
+
+		public void loop()
+		{
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		}
+
+		public void stop()
+		{
+			clip.stop();
 		}
 	}
 }
